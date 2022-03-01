38eb4f87be8f95b9804ee0bbee7197b0fcb31bae
dao <gary.vo32@gmail.com>
2022-02-28 18:56:59 -0800

    created and connected index.html and main.js; also set up functions to be defined within main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+/*
+** playerChoice() will get the users selection and return warrior, mage, or archer
+**
+** arg(s):      none
+** return(s):   string type fighter; ex. 'warrior'
+*/
+function playerChoice() {
+    //
+}
+
+/*
+** computerChoice() will randomly generate a number to pick and return 1 of the 3 options
+**
+** arg(s):      none
+** return(s):   string type figher; ex. 'mage'
+*/
+
+/*
+** playRound(fighter1, fighter2) take the computer and players selection and
+** determine the winner of the round.
+**
+** arg(s):      strings type 'fighter1' & 'fighter2'
+** return(s):   number type 'winner'; number 1 correlate to the player while 
+**              number 2 will represent the computer
+*/
+
+/*
+** game() will loop through and keep track of the score and will end once the
+** first one to 5 wins is determined.
+**
+** arg(s):      none
+** return(s):   none
+*/
