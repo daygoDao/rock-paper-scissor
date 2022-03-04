c06e02ffed209b00545f7e0dd29d43c422516bfd
dao <gary.vo32@gmail.com>
2022-03-03 20:08:21 -0800

    idk

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ document.getElementById('mage').addEventListener('click', chosenMage);
 document.getElementById('archer').addEventListener('click', chosenArcher);
 
 function chosenWarrior() {
-    document.querySelector('#warrior').style.background = 'blue';
-    playerSelected = 'warrior';
+	document.querySelector('#warrior').style.background = 'blue';
+	playerSelected = 'warrior';
 }
 
 function chosenMage() {
-    document.querySelector('#mage').style.background = 'blue';
+	document.querySelector('#mage').style.background = 'blue';
 }
 
 function chosenArcher() {
-    document.querySelector('#archer').style.background = 'blue';
+	document.querySelector('#archer').style.background = 'blue';
 }
 
 /*
@@ -24,10 +24,10 @@ function chosenArcher() {
  ** return(s):   string type fighter; ex. 'warrior'
  */
 function playerChoice() {
-    //get user input
-    let choice = prompt('choose warrior, mage, or archer', '').toLowerCase();
-    //return string value
-    return choice;
+	//get user input
+	let choice = prompt('choose warrior, mage, or archer', '').toLowerCase();
+	//return string value
+	return choice;
 }
 
 /*
@@ -37,15 +37,30 @@ function playerChoice() {
  ** return(s):   string type figher; ex. 'mage'
  */
 function computerChoice() {
-    let fighters = ['warrior', 'mage', 'archer'];
-    let computerChoice = Math.floor(Math.random() * 3);
-    console.log(fighters[computerChoice]);
-    return fighters[computerChoice];
+	let fighters = ['warrior', 'mage', 'archer'];
+	let computerChoice = Math.floor(Math.random() * 3);
+	console.log(fighters[computerChoice]);
+	return fighters[computerChoice];
 }
 
 /*
- ** playRound(fighter1, fighter2) take the computer and players selection and
- ** determine the winner of the round.
+ ** playRound() will call the helper function battle() returns the winner.
+ ** 
+ **
+ ** arg(s):      none
+ ** return(s):   none
+ */
+function playRound() {
+	let fighter1 = playerChoice();
+	let fighter2 = computerChoice();
+	console.log(`player 1 fighter is ${fighter1}`)
+	console.log(`player 2 fighter is ${fighter2}`)
+	//    console.log(battle(fighter1, fighter2));
+	return battle(fighter1, fighter2);
+}
+/*
+ ** battle(fighter1, fighter2) take the computer and players selection as
+ ** arguments and determines the winner of the round.
  ** Warrior > Archer
  ** Archer > Mage
  ** Mage > Warrior
@@ -54,39 +69,30 @@ function computerChoice() {
  ** return(s):   number type 'winner'; number 1 correlate to the player while 
  **              number 2 will represent the computer
  */
-function playRound() {
-    let fighter1 = playerChoice();
-    let fighter2 = computerChoice();
-    console.log(`player 1 fighter is ${fighter1}`)
-    console.log(`player 2 fighter is ${fighter2}`)
-//    console.log(battle(fighter1, fighter2));
-    return battle(fighter1, fighter2);
-}
-//helper function to fight!
 function battle(fighter1, fighter2) {
-    let winner = 0;
-    if (fighter1 === fighter2) {
-        console.log('tie');
-    } else if (fighter1 === 'warrior' && fighter2 === 'archer') {
-        console.log("player 1's warrior murders the archer with the swing of his axe");
-        winner = 1;
-    } else if (fighter1 === 'archer' && fighter2 === 'mage') {
-        console.log("player 1's archer launches an arrow into the mage's skull");
-        winner = 1;
-    } else if (fighter1 === 'mage' && fighter2 === 'warrior') {
-        console.log("player 1's mage renders the warrior frozen to death with his magical blizzard");
-        winner = 1;
-    } else if (fighter2 === 'warrior' && fighter1 === 'archer') {
-        console.log("computer's warrior murders the archer with the swing of his axe");
-        winner = 2;
-    } else if (fighter2 === 'archer' && fighter1 === 'mage') {
-        console.log("computer's archer launches an arrow into the mage's skull");
-        winner = 2;
-    } else if (fighter2 === 'mage' && fighter1 === 'warrior') {
-        console.log("computer's mage renders the warrior frozen to death with his magical blizzard");
-        winner = 2;
-    }
-    return winner;
+	let winner = 0;
+	if (fighter1 === fighter2) {
+		console.log('tie');
+	} else if (fighter1 === 'warrior' && fighter2 === 'archer') {
+		console.log("player 1's warrior murders the archer with the swing of his axe");
+		winner = 1;
+	} else if (fighter1 === 'archer' && fighter2 === 'mage') {
+		console.log("player 1's archer launches an arrow into the mage's skull");
+		winner = 1;
+	} else if (fighter1 === 'mage' && fighter2 === 'warrior') {
+		console.log("player 1's mage renders the warrior frozen to death with his magical blizzard");
+		winner = 1;
+	} else if (fighter2 === 'warrior' && fighter1 === 'archer') {
+		console.log("computer's warrior murders the archer with the swing of his axe");
+		winner = 2;
+	} else if (fighter2 === 'archer' && fighter1 === 'mage') {
+		console.log("computer's archer launches an arrow into the mage's skull");
+		winner = 2;
+	} else if (fighter2 === 'mage' && fighter1 === 'warrior') {
+		console.log("computer's mage renders the warrior frozen to death with his magical blizzard");
+		winner = 2;
+	}
+	return winner;
 }
 
 /*
@@ -97,20 +103,20 @@ function battle(fighter1, fighter2) {
  ** return(s):   none
  */
 function game() {
-    let exitGame = false;
-    let playerScore = 0;
-    let computerScore = 0;
-    while (!exitGame) {
-        let winner = playRound();
-        if (winner === 1) {
-            playerScore++;
-        }
-        if (winner === 2) {
-            computerScore++;
-        }
-        if (playerScore === 5 || computerScore === 5) {
-            exitGame = true;
-        }
-        console.log(`current score is player:${playerScore} - computer:${computerScore}`);
-    }
+	let exitGame = false;
+	let playerScore = 0;
+	let computerScore = 0;
+	while (!exitGame) {
+		let winner = playRound();
+		if (winner === 1) {
+			playerScore++;
+		}
+		if (winner === 2) {
+			computerScore++;
+		}
+		if (playerScore === 5 || computerScore === 5) {
+			exitGame = true;
+		}
+		console.log(`current score is player:${playerScore} - computer:${computerScore}`);
+	}
 }
