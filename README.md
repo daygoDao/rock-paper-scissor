1b0394c2218eea8849683e08dc14f3f5d2cec8da
dao <gary.vo32@gmail.com>
2022-03-06 14:46:10 -0800

    finished functional UI along with the game. need polish with pictures a bit more love

--- README.md
+++ README.md
@@ -6,14 +6,15 @@ Created by Dao Vo
 
 ## pseudocode:
 * Game begins when user loads into the webpage.
-* When user clicks on one of the three buttons that action will be recorded as one of five rounds.
+* When user clicks on one of the three buttons that action will be recorded on the scoreboard
   * add event listeners for the 3 choices that holds the function  playRound
-* choices selected buy user and opponent will be displayed below in a div along with results.
+* choices selected by user and opponent will be displayed below in a div along with results.
     * in the 1v1 section winner will have a background of green and the loser with red.
 * when user finishes five rounds then the game will end with the score and option to play again.
 
 ## todo
-- [ ] add images
-- [ ] get game to initiate
-- [ ] make start game pop up when user loads into the page
+- [ ] find and add images
+- [ ] add ui section where fighters chosen will appear each round with winner & loser highlighted
+- [x] game is function with UI
+- [x] get game to initiate
 - [x] have fun
--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-let playerSelected = 'n/a';
+console.log('welcome to the deathmatch!');
+let playerScore = 0;
+let computerScore = 0;
+let playerScoreboard = document.querySelector('.playerScoreboard');
+let npcScoreboard = document.querySelector('.computerScoreboard');
+let roundResult = document.querySelector('.roundResult');
 
 const rock = document.getElementById('rock')
 rock.addEventListener('click', playRound);
@@ -11,13 +16,10 @@ scissor.addEventListener('click', playRound);
  ** playerChoice() will get the users selection and return warrior, mage, or archer
  **
  ** arg(s):      none
- ** return(s):   string type fighter; ex. 'warrior'
+ ** return(s):   string type fighter; ex. 'rock'
  */
 function playerChoice(e) {
-	//get user input
 	let choice = e.target.value;
-
-	//return string value
 	return choice;
 }
 
@@ -25,7 +27,7 @@ function playerChoice(e) {
  ** computerChoice() will randomly generate a number to pick and return 1 of the 3 options
  **
  ** arg(s):      none
- ** return(s):   string type figher; ex. 'mage'
+ ** return(s):   string type figher; ex. 'paper'
  */
 function computerChoice() {
 	let fighters = ['rock', 'paper', 'scissor'];
@@ -44,9 +46,22 @@ function computerChoice() {
 function playRound(e) {
 	let fighter1 = playerChoice(e);
 	let fighter2 = computerChoice();
+	let winner = battle(fighter1, fighter2);
 	console.log(`player 1 fighter is ${fighter1}`)
 	console.log(`player 2 fighter is ${fighter2}`)
-	return battle(fighter1, fighter2);
+
+	console.log(winner);
+	if (winner === 1) {
+		playerScore++;
+		playerScoreboard.textContent = playerScore;
+	}
+	if (winner === 2) {
+		computerScore++;
+		npcScoreboard.textContent = computerScore;
+	}
+	console.log(`current score is player:${playerScore} - computer:${computerScore}`);
+
+	checkWinner(winner);
 }
 
 /*
@@ -63,52 +78,45 @@ function playRound(e) {
 function battle(fighter1, fighter2) {
 	let winner = 0;
 	if (fighter1 === fighter2) {
-		console.log('tie');
+		roundResult.textContent = 'tie';
 	} else if (fighter1 === 'rock' && fighter2 === 'scissor') {
-		console.log("player 1's warrior murders the archer with the swing of his axe");
+		roundResult.textContent = "player 1's warrior murders the archer with the swing of his axe";
 		winner = 1;
 	} else if (fighter1 === 'scissor' && fighter2 === 'paper') {
-		console.log("player 1's ninja throws down a smoke bomb and throws a kunai into the mage's skull");
+		roundResult.textContent = "player 1's ninja throws down a smoke bomb and throws a kunai into the mage's skull";
 		winner = 1;
 	} else if (fighter1 === 'paper' && fighter2 === 'rock') {
-		console.log("player 1's mage renders the warrior frozen to death with his magical blizzard");
+		roundResult.textContent = "player 1's mage renders the warrior frozen to death with his magical blizzard";
 		winner = 1;
 	} else if (fighter2 === 'rock' && fighter1 === 'scissor') {
-		console.log("computer's warrior murders the archer with the swing of his axe");
+		roundResult.textContent = "computer's warrior murders the archer with the swing of his axe";
 		winner = 2;
 	} else if (fighter2 === 'scissor' && fighter1 === 'paper') {
-		console.log("computer's ninja sneaks through the shadows and backstabs the mage");
+		roundResult.textContent = "computer's ninja sneaks through the shadows and backstabs the mage";
 		winner = 2;
 	} else if (fighter2 === 'paper' && fighter1 === 'rock') {
-		console.log("computer's mage renders the warrior frozen to death with his magical blizzard");
+		roundResult.textContent = "computer's mage renders the warrior frozen to death with his magical blizzard";
 		winner = 2;
 	}
 	return winner;
 }
 
 /*
- ** game() will loop through and keep track of the score and will end once the
- ** first one to 5 wins is determined.
+ ** checkWinner() will see if someone is first to 5 wins
  **
- ** arg(s):      none
+ ** arg(s):      number type 'winner'; 0 is a tie, 1 will be the user, 2 will be the computer
  ** return(s):   none
  */
-function game() {
-	let exitGame = false;
-	let playerScore = 0;
-	let computerScore = 0;
-	while (!exitGame) {
-		let winner = playRound();
-		console.log(winner);
-		if (winner === 1) {
-			playerScore++;
-		}
-		if (winner === 2) {
-			computerScore++;
-		}
-		if (playerScore === 5 || computerScore === 5) {
-			exitGame = true;
-		}
-		console.log(`current score is player:${playerScore} - computer:${computerScore}`);
+//this function will check if game is over with the first one to 5 wins
+function checkWinner(winner) {
+	if (playerScore === 5 || computerScore === 5) {
+		alert(`the winner is ${winner}. but for how long?`);
+		//reset score to 0
+		playerScore = 0;
+		computerScore = 0;
+		playerScoreboard.textContent = playerScore;
+		npcScoreboard.textContent = computerScore;
+		roundResult.textContent = 'WHO SHALL BE THE VICTOR?!'
+
 	}
 }
